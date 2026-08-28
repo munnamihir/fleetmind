1e4b7838ecab95cc241a222accfcb74ee4ac791b
Mihir Bommisetty <38711080+munnamihir@users.noreply.github.com>
2026-08-28 00:02:39 -0400

    feat: wire contextual selection inspector

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { FleetMindExperience } from './FleetMindExperience';
+import { FleetMindSelectionInspector } from './FleetMindSelectionInspector';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <FleetMindExperience />
+    <FleetMindSelectionInspector />
     <App />
   </React.StrictMode>,
 );
