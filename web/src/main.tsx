47893423ab0f4561193bf397f27bdabb438a9991
Mihir Bommisetty <38711080+munnamihir@users.noreply.github.com>
2026-08-28 00:08:31 -0400

    feat: wire intent-based navigation rail

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { FleetMindBreadcrumbs } from './FleetMindBreadcrumbs';
 import { FleetMindExperience } from './FleetMindExperience';
+import { FleetMindNavigationRail } from './FleetMindNavigationRail';
 import { FleetMindSelectionInspector } from './FleetMindSelectionInspector';
 import { FleetMindWorkInbox } from './FleetMindWorkInbox';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <FleetMindNavigationRail />
     <FleetMindExperience />
     <FleetMindBreadcrumbs />
     <FleetMindWorkInbox />
