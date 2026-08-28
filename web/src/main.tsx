3c598ea0fedae421bf77342a197c32b23934ead5
Mihir Bommisetty <38711080+munnamihir@users.noreply.github.com>
2026-08-28 00:04:38 -0400

    feat: wire operator work inbox

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { FleetMindExperience } from './FleetMindExperience';
 import { FleetMindSelectionInspector } from './FleetMindSelectionInspector';
+import { FleetMindWorkInbox } from './FleetMindWorkInbox';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <FleetMindExperience />
+    <FleetMindWorkInbox />
     <FleetMindSelectionInspector />
     <App />
   </React.StrictMode>,
