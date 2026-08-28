ff1bba916af488f194cc9e961ff2ef092d166cd7
Mihir Bommisetty <38711080+munnamihir@users.noreply.github.com>
2026-08-28 00:01:03 -0400

    feat: wire FleetMind UX experience layer

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { FleetMindExperience } from './FleetMindExperience';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <FleetMindExperience />
     <App />
   </React.StrictMode>,
 );
